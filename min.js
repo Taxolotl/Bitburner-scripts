f591fc138b7f0ce69b3beeb5997d991285301359
Taxolotl <112824633+Taxolotl@users.noreply.github.com>
2023-08-12 03:24:42 -0400

    Update min.js

--- min.js
+++ min.js
@@ -1,20 +1,4 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.killall("HowmanyYY")
-  ns.deleteServer("HowmanyYY");
-
-  ns.killall("Howmany");
-  ns.deleteServer("Howmany");
-
-  ns.killall("Howmany-0");
-  ns.deleteServer("Howmany-0");
-
-  ns.killall("Howmany-1");
-  ns.deleteServer("Howmany-1");
-
-  ns.killall("Howmany-2");
-  ns.deleteServer("Howmany-2");
-
-  ns.killall("Howmany-3");
-  ns.deleteServer("Howmany-3");
+  //idk why min.js, just was the first thing to come into my mind lol.
 } 
